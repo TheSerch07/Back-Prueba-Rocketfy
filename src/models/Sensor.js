71bbb15576505cc5320713fb4d7aa187e8c1006f
TheSerch07 <jejog50@gmail.com>
2023-10-28 15:30:39 -0500

    remove model Sensor

--- src/models/Sensor.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require('mongoose');
-
-const sensorSchema = new Schema({
-    sensor_id: Number,
-    sensor_name: String,
-    data: Array
-})
-
-module.exports = model('Sensor', sensorSchema);
