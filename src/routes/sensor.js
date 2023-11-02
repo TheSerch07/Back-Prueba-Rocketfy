370aa591755d453e3d2ce911033f718bb0d49087
TheSerch07 <jejog50@gmail.com>
2023-11-02 15:36:38 -0500

    remove socket

--- src/routes/sensor.js
+++ src/routes/sensor.js
@@ -3,15 +3,6 @@ const mongoose = require('mongoose');
 const router = Router();
 const Sensor = mongoose.connection.collection('sensor');
 const socketIo = require('socket.io');
-const io = socketIo();
-
-io.on('connection', (socket) => {
-    console.log('A client has connected to Socket.io');
-});
-
-router.get('/connect-to-socket', (req, res) => {
-    res.sendFile(process.cwd() + '/client/index.html'); // Debes crear este archivo HTML
-});
 
 router.get('/:id', async (req, res) => {
     const id = Number(req.params.id);
